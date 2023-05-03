0667c666bade82920a9accb30380412f71eafc51
Alessandro <alssdev3@gmail.com>
2023-05-03 12:27:23 -0600

    feat(establecimientos): edit implemented

---
+++ pages/establecimientos/_id/edit.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <h1 class="title">Editar el establecimiento
+      <span class="has-text-primary">ID{{ this.idest }}</span>
+    </h1>
+
+    <b-field label="Nombre">
+      <b-input v-model="establecimiento.nombre" placeholder="Nombre del establecimiento" maxlength="50" />
+    </b-field>
+
+    <b-field label="Dirección">
+      <b-input v-model="establecimiento.direccion" placeholder="Ciudad" type="textarea" maxlength="200" />
+    </b-field>
+
+    <b-field label="Municipio">
+      <input-municipio @select="selectMunicipio" />
+    </b-field>
+
+    <div class="buttons mt-6">
+      <b-button icon-left="arrow-left" @click="$goBack">Regresar</b-button>
+      <b-button icon-left="content-save" type="is-primary" @click="confirm">Guardar</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    idest: null,
+    establecimiento: {
+      nombre: '',
+      direccion: '',
+      idmunicipio: null
+    },
+    nombreMuni: 'prueba de nombre'
+  }),
+
+  created () {
+    if (!isNaN(this.$route.params.id)) {
+      this.idest = this.$route.params.id;
+      this.fetchData();
+    } 
+  },
+
+  methods : {
+    async pushData () {
+      try {
+        const body = this.prepareBodyRequest();
+        await this.$axios.put(`/establecimientos/${this.idest}`, body);
+        
+        this.$buefy.notification.open({
+          type: 'is-success',
+          message: 'El establecimiento ha sido editado con éxito',
+          duration: 5000
+        });
+        this.$router.go(-1);
+      } catch (error) {
+        this.$errorHandler(error);
+      }
+    },
+    prepareBodyRequest () {
+      return {
+        nombre: this.establecimiento.nombre,
+        direccion: this.establecimiento.direccion,
+        idmunicipio: this.establecimiento.idmunicipio
+      };
+    },
+    confirm () {
+      if (this.validateData()) {
+        this.$buefy.dialog.confirm({
+          title: 'Confirmación',
+          message: `Antes de completar el proceso debe 
+          confirmar que está seguro ¿Desea continuar?`,
+          onConfirm: this.pushData,
+          hasIcon: true,
+          type: 'is-success',
+        });
+      }
+    },
+    validateData () {
+      return true;
+    },
+
+    async fetchData () {
+      try {
+        const response = await this.$axios.$get(`/establecimientos/${this.idest}`);
+        this.establecimiento = response.list[0];
+        // this.nombreMuni = ... PENDIENTE
+      } catch (error) {
+        this.$errorHandler(error);
+      }
+    },
+
+    selectMunicipio (option) {
+      if (option.idmunicipio !== null) {
+        this.establecimiento.idmunicipio = option.idmunicipio;
+      } else {
+        this.establecimiento.idmunicipio = null;
+      }
+    }
+  }
+}
+</script>
