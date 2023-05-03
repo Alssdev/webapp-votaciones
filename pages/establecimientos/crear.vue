3552e708e832a597a118809d398940fc1917ce60
Alessandro <alssdev3@gmail.com>
2023-05-03 12:13:19 -0600

    feat(establecimientos): create implemented

--- pages/establecimientos/crear.vue
+++ pages/establecimientos/crear.vue
@@ -11,12 +11,70 @@
     </b-field>
 
     <b-field label="Municipio">
-      <input-municipio />
+      <input-municipio @select="selectMunicipio" />
     </b-field>
 
     <div class="buttons mt-6">
-      <b-button icon-left="arrow-left">Regresar</b-button>
-      <b-button icon-left="check-bold" type="is-primary">Crear</b-button>
+      <b-button icon-left="arrow-left" @click="$goBack">Regresar</b-button>
+      <b-button icon-left="check-bold" type="is-primary" @click="confirm">Crear</b-button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    nombre: '',
+    direccion: '',
+    idmunicipio: null
+  }),
+
+  methods : {
+    async pushData () {
+      try {
+        const body = this.prepareBodyRequest();
+        await this.$axios.post('/establecimientos', body);
+        
+        this.$buefy.notification.open({
+          type: 'is-success',
+          message: 'El establecimiento ha sido creado con éxito',
+          duration: 5000
+        });
+        this.$router.go(-1);
+      } catch (error) {
+        this.$errorHandler(error);
+      }
+    },
+    prepareBodyRequest () {
+      return {
+        nombre: this.nombre,
+        direccion: this.direccion,
+        idmunicipio: this.idmunicipio
+      };
+    },
+    confirm () {
+      if (this.validateData()) {
+        this.$buefy.dialog.confirm({
+          title: 'Confirmación',
+          message: `Antes de completar el proceso debe 
+          confirmar que está seguro ¿Desea continuar?`,
+          onConfirm: this.pushData,
+          hasIcon: true,
+          type: 'is-success',
+        });
+      }
+    },
+    validateData () {
+      return true;
+    },
+
+    selectMunicipio (option) {
+      if (option.idmunicipio !== null) {
+        this.idmunicipio = option.idmunicipio;
+      } else {
+        this.idmunicipio = null;
+      }
+    }
+  }
+}
+</script>
